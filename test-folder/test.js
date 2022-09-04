9bae3f0f2ad017eaba85a29d7b2386bf06d0e71b
sanjiv576 <sanjiv576>
2022-09-04 22:35:55 +0545

    testing

---
+++ test-folder/test.js
@@ -0,0 +1,4 @@
+const dateFormat = require('simplified-dateformat');
+console.log(dateFormat.getDate('-Nepali'));
+console.log(dateFormat.getDate('-English'));
+console.log(dateFormat.getDate('Chinese'));
