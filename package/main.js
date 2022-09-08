9037f50a78a8bbf40fde26e1fafba0bed3e0dc6e
sanjiv576 <sanjiv576>
2022-09-08 17:19:44 +0545

    fixed some bugs and optimized

--- package/main.js
+++ package/main.js
@@ -1,24 +1,39 @@
+
+const languages = require('./languages.json');
+
 exports.getDate = function (lang) {
     const today = new Date();
-    const _ = require('lodash');
+
     const options = {
         year: 'numeric',
         month: 'long',
-        weekday: 'short'
+        weekday: 'short',
+        day: 'numeric'
     };
 
-    const reqLang = _.lowerCase(lang);
+    var bcpCode = '';
 
-    if (reqLang === 'english') {
-        return today.toLocaleString('en-US', options);
-    }
-    else if (reqLang === 'chinese') {
-        return today.toLocaleString('zh-TW', options);
-    }
-    else if (reqLang === 'nepali') {
-        return today.toLocaleString('ne-NP', options);
-    }
-    else {
+    if (lang === '') {
         return "invalid entry";
     }
+    else {
+
+        Object.entries(languages).forEach(function ([key, value]) {
+
+            if (lang.toLowerCase() === key.toLowerCase()) {
+                bcpCode = value;
+
+            }
+        });
+
+        if (bcpCode !== '') {
+            return today.toLocaleString(bcpCode, options);
+        }
+        else {
+            return `${lang} is not available.`;
+        }
+
+    }
+
 }
+
