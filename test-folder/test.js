81df12ddd417f86c0f9a716edd85526ed43cc099
sanjiv576 <sanjiv576>
2022-09-08 17:41:31 +0545

    95 languages added

--- test-folder/test.js
+++ test-folder/test.js
@@ -1,4 +1,16 @@
 const dateFormat = require('simplified-dateformat');
-console.log(dateFormat.getDate('-Nepali'));
-console.log(dateFormat.getDate('-English'));
-console.log(dateFormat.getDate('Chinese'));
+
+console.log(dateFormat.getDate('Nepali'));
+console.log(dateFormat.getDate('English'));
+console.log(dateFormat.getDate('Thai'));
+console.log(dateFormat.getDate('arabic'));
+console.log(dateFormat.getDate('grEEk'));
+console.log(dateFormat.getDate('JapAneSe'));
+
+console.log(dateFormat.getDate('Hindi'));
+console.log(dateFormat.getDate('telugu'));
+console.log(dateFormat.getDate('kANuri'));
+console.log(dateFormat.getDate('chiNese'));
+
+
+
